0d3d2d82ef6d8f15c7dff3b2a3378b4c2ba4bfab
wiktorlazarski <wjlazarski@gmail.com>
2020-04-02 15:54:41 +0200

    lowerizing and uppercasting examples

--- 1.modify-bash script/modify_example.sh
+++ 1.modify-bash script/modify_example.sh
@@ -1,4 +1,67 @@
 #!/bin/bash
 # Created by Wiktor Lazarski 02/04/2020
 
-./modify.sh -h
+# Example 1: Display help function
+echo "------------------------------ PRINT HELP ----------------------------"
+echo "	./modify.sh -h"
+
+#./modify.sh -h
+
+echo ""
+
+echo "------------------------------ -l/-u lowerize uppercast --------------"
+echo "					Step 1: Create file tree structure."
+# create file tree structure
+mkdir test
+touch test.txt
+mkdir test/test
+touch test/test.txt
+mkdir test/test/test
+touch test/test/test.txt
+
+# display file tree structure
+tree --noreport 
+echo ""
+
+# perform non-recursive uppercasting
+echo "					Step 2: Perform non-recursive uppercasting"
+echo "./modify.sh -u test test.txt"
+
+./modify.sh -u test test.txt
+
+echo "File tree structure:"
+tree --noreport
+echo ""
+
+# perform recursive uppercasting
+echo "					Step 3: Perform recursive uppercasting"
+echo "./modify.sh -r -u TEST"
+./modify.sh -r -u TEST
+
+echo "File tree structure:"
+tree --noreport
+echo ""
+
+# perform non-recursive lowerizing 
+echo "					Step 4: Perform non-recursive lowerizing"
+echo "./modify.sh -l TEST TEST.txt"
+
+./modify.sh -l TEST TEST.txt
+
+echo "File tree structure:"
+tree --noreport
+echo ""
+
+# perform recursive lowerizing
+echo "					Step 5: Perform recursive lowerizing"
+echo "./modify.sh -r -l TEST"
+./modify.sh -r -l test
+
+echo "File tree structure:"
+tree --noreport
+echo ""
+
+echo "---------------------------- <sed pattern> modification --------------"
+
+# remove file tree structure
+rm -r test test.txt
