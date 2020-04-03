fda4fb6cc39104859c5808fc35d8221d88d64975
wiktorlazarski <wjlazarski@gmail.com>
2020-04-04 01:18:08 +0200

    Transformed from bash to sh

--- 1.modify-bash script/modify.sh
+++ 1.modify-bash script/modify.sh
@@ -1,20 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 # Created by Wiktor Lazarski 02/04/2020
 
 script_name=$(basename $0)
-declare -a files_arr
-declare -r LOWERCASE_SED_PATTERN="s/.*/\L&/"
-declare -r UPPERCASE_SED_PATTERN="s/.*/\U&/"
-declare -r UNSPECIFIED_SED_PATTERN="unspecified"
+LOWERCASE_SED_PATTERN="s/.*/\L&/"
+UPPERCASE_SED_PATTERN="s/.*/\U&/"
+UNSPECIFIED_SED_PATTERN="unspecified"
 
 # function for printing an error
-function error_msg()
+error_msg()
 {
 	echo "$script_name: error: $1" 1>&2
 }
 
 # function for desplaying help
-function display_help()
+display_help()
 {
 	cat<<EOT
 NAME
@@ -49,53 +48,26 @@ AUTHOR
 EOT
 }
 
-# do with command line arguments
-recursive=n
-sed_pattern=$UNSPECIFIED_SED_PATTERN
-while [ "x$1" != "x" ]
-do
-	case "$1" in
-		-r) recursive=y;;
-		-l) sed_pattern=$LOWERCASE_SED_PATTERN;;
-		-u) sed_pattern=$UPPERCASE_SED_PATTERN;;
-		-h) display_help; exit 0;;
-		-*) error_msg "bad option $1"; exit 1;;
-		*)
-			if [ "$sed_pattern" = "$UNSPECIFIED_SED_PATTERN" ]; then
-				sed_pattern=$1
-			else 
-				files_arr+=($1)
-			fi;;
-	esac
-
-	shift
-done
-
-# check correctness of sed pattern
-if ! [[ $sed_pattern =~ s/.+/.*/$ ]]; then
-	error_msg "invalid sed pattern specified"
-	exit 1
-fi
-
 # function renames file according to currently set sed pattern
-function rename_file()
+rename_file()
 {
 	# get file name without path
 	old_name=$(basename $1)
-	file_extension=$(echo ".${old_name#*.}")
-	if [ -d $1 ]; then
-		file_extension=""
+	
+	file_extension=""
+	if ! [ -d $1 ]; then
+		file_extension=$(echo $old_name | sed "s/.*\./""/")
+		file_extension=$(echo ".$file_extension")
+		
+		old_name=$(echo $old_name | sed "s/$file_extension/""/")
 	fi	
-	
-	# remove file extension	 
-	old_name=$(echo ${old_name/$file_extension/""})
-	
+
 	new_name=$(echo $old_name | sed $sed_pattern)
 	# check if renaming needed
 	if [ "$old_name" = "$new_name" ]; then
 		return
 	fi
-
+	
 	# add directory path and extension to a new file name
 	new_name="$(dirname $1)/$(echo "$new_name$file_extension")"
 	
@@ -103,7 +75,7 @@ function rename_file()
 	mv $1 $new_name 
 }
 
-function recursive_rename()
+recursive_rename()
 {
 	if [ $recursive = 'y' ] && [ -d $1 ]; then
 		# iterate over all files in a passed directory
@@ -120,8 +92,32 @@ function recursive_rename()
 	rename_file $1
 }
 
-# start renaming algorithm
-for passed_file in "${files_arr[@]}"
+# do with command line arguments
+recursive=n
+sed_pattern=$UNSPECIFIED_SED_PATTERN
+while [ "x$1" != "x" ]
 do
-	recursive_rename $passed_file 
+	case "$1" in
+		-r) recursive=y;;
+		-l) sed_pattern=$LOWERCASE_SED_PATTERN;;
+		-u) sed_pattern=$UPPERCASE_SED_PATTERN;;
+		-h) display_help; exit 0;;
+		-*) error_msg "bad option $1"; exit 1;;
+		*)
+			if [ "$sed_pattern" = "$UNSPECIFIED_SED_PATTERN" ]; then
+				sed_pattern=$1
+			
+				# check correctness of sed pattern
+				is_correct=$(echo "$sed_pattern" | sed "s/s\/.\+\/.*\/$//")
+				if ! [ "$is_correct" = "" ]; then
+					error_msg "incorrect <sed pattern> specified: $sed_pattern"
+					exit 1
+				fi
+			
+			else
+				recursive_rename $1
+			fi;;
+	esac
+
+	shift
 done
--- 1.modify-bash script/modify_example.sh
+++ 1.modify-bash script/modify_example.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 # Created by Wiktor Lazarski 02/04/2020
 
-
-function display_tree_structure()
+display_tree_structure()
 {
 	echo "File tree structure:"
 	tree --noreport
@@ -34,14 +33,13 @@ echo "					Step 2: Perform non-recursive uppercasting"
 echo "./modify.sh -u test test.txt"
 
 ./modify.sh -u test test.txt
-
 display_tree_structure
 
 # perform recursive uppercasting
 echo "					Step 3: Perform recursive uppercasting"
 echo "./modify.sh -r -u TEST"
-./modify.sh -r -u TEST
 
+./modify.sh -r -u TEST
 display_tree_structure
 
 # perform non-recursive lowerizing 
@@ -49,32 +47,34 @@ echo "					Step 4: Perform non-recursive lowerizing"
 echo "./modify.sh -l TEST TEST.txt"
 
 ./modify.sh -l TEST TEST.txt
-
 display_tree_structure
 
 # perform recursive lowerizing
 echo "					Step 5: Perform recursive lowerizing"
-echo "./modify.sh -r -l TEST"
-./modify.sh -r -l test
+echo "./modify.sh -r -l test"
 
+./modify.sh -r -l test
 display_tree_structure
 
 echo "---------------------------- <sed pattern> modification --------------"
 echo "					Step 1: Replace first t into WUT in test and test.txt non-recursively"
 echo "./modify.sh s/t/WUT/ test test.txt" 
 ./modify.sh s/t/WUT/ test test.txt
+echo ""
 
 display_tree_structure
 
 echo "					Step 2: Replace first t into WUT in test recursively"
-echo "./modify.sh -r s/t/WUT/ test" 
+echo "./modify.sh -r s/t/WUT/ WUTest" 
 ./modify.sh -r s/t/WUT/ WUTest
+echo ""
 
 display_tree_structure
 
 echo "					Step 3: Replace first WUT into t in all files recursively"
 echo "./modify.sh -r s/WUT/t/ WUTesWUT WUTest.txt" 
 ./modify.sh -r s/WUT/t/ WUTesWUT WUTest.txt
+echo ""
 
 display_tree_structure
 
@@ -82,10 +82,12 @@ echo "					Step 4: Wrong pattern specified by the user"
 echo "Missing '/' at the end of sed pattern."
 echo "./modify.sh -r s/WUT/t tesWUT" 
 ./modify.sh -r s/WUT/t tesWUT
+echo ""
 
 echo "Missing 's' at the beginning of sed pattern."
 echo "./modify.sh -r WUT/WUT/t/ tesWUT" 
 ./modify.sh -r WUT/WUT/t/ tesWUT 
+echo ""
 
 echo "No first sed pattern argument specified"
 echo "./modify.sh -r s//t/ tesWUT" 
