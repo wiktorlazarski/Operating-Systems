695ecebeb5cc0c96f0486f642a5d3e3ab6893c6e
wiktorlazarski <wjlazarski@gmail.com>
2020-04-19 15:40:12 +0200

    set up and clean moss simulator scripts

---
+++ 3.scheduling/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -r task3
+
+# display final message
+echo "MOSS Scheduling Simulator removed" 
--- 3.scheduling/setup_sim.sh
+++ 3.scheduling/setup_sim.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# add folder to a classpath
+CLASSPATH=.:$CLASSPATH
+export CLASSPATH
+
+# unpack files
+tar -xf task3.tgz
+cd task3/ftp
+./setUp
+
+# display final message
 echo "Setting up simulator"
