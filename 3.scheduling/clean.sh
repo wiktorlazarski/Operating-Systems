415c4d72e7299c2b153b2e574843a3c33b0cc7de
wiktorlazarski <wjlazarski@gmail.com>
2020-05-02 14:21:01 +0200

    Script for performing lab task and cleaning

--- 3.scheduling/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -r task3
-
-# display final message
-echo "MOSS Scheduling Simulator removed" 
---
+++ 3.scheduling/perform_task.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# sets up simulator for experiments
+function setup_simulator()
+{
+	# add folder to a classpath
+	CLASSPATH=.:$CLASSPATH
+	export CLASSPATH
+
+	# unpack files
+	tar -xf task3.tgz
+	cd task3/ftp
+	./setUp
+
+	# jump out of folder
+	cd ../..
+
+	# display final message
+	echo "Setting up simulator - DONE"
+}
+
+# cleans experiment results and simulator env
+function clean()
+{
+	rm -r task3 results
+
+	# display final message
+	echo "Cleaning - DONE" 
+}
+
+# performs set of experiments and saves in results folder
+function task()
+{
+	SCHEDULER_CLASSNAME="Scheduling"
+	SUMMARY_PROCESSES_PATH="./task3/work/Summary-Processes"	
+	SUMMARY_RESULTS_PATH="./task3/work/Summary-Results"
+	CONFIGS_PATH="./configurations"
+
+	mkdir results
+
+	for file in "$CONFIGS_PATH"/*
+	do	
+		basename=$(basename $file)
+
+		cd "task3/work"
+		java $SCHEDULER_CLASSNAME "../../configurations/$basename"
+		cd "../.."
+
+		no_extension_basename=$(echo $basename | sed "s/\..*//")
+		output_file="./results/$no_extension_basename-results.txt"
+		touch $output_file
+
+		cat $SUMMARY_PROCESSES_PATH > $output_file
+		cat $SUMMARY_RESULTS_PATH >> $output_file 
+	done
+	
+	echo "Task completed. Results are READY."
+}
+
+# lab script
+if [ "$1" == "clean" ]; then
+	clean
+else
+	setup_simulator
+	task
+fi
