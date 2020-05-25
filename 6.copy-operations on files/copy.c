8dfe361b15252a88266656966a207efa2b7d80f2
wiktorlazarski <wjlazarski@gmail.com>
2020-05-25 14:33:09 +0200

    program structure

--- 6.copy-operations on files/copy.c
+++ 6.copy-operations on files/copy.c
@@ -1,9 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#define SUCCESS 0
+#define FAILED 1
+
+void help();
+int copy(const char *src, const char *dst);
+int m_copy(const char *src, const char *dst);
 
 int main(int argc, char *argv[])
 {
-	printf("Hello copy\n");
+	int opt;
+	while((opt = getopt(argc, argv, ":m:hx")) != -1){
+		switch(opt){
+			case 'h':
+				help();
+				return 0;
+			case 'm':
+				return m_copy("from", "to");
+			case ':':
+				printf("ERROR: source file required\n");	
+				exit(FAILED);
+			case '?':
+				printf("ERROR: unknown operation: %c\n", optopt);
+				exit(FAILED);
+		}
+	}
+	
+	return copy("from", "to");
+}
 
-	return 0;
+void help(){
+	printf("HELP!!!\n");
+}
+
+int copy(const char *src, const char *dst){
+	printf("READ/WRITE coping\n");
+	return SUCCESS;
+}
+
+int m_copy(const char *src, const char *dst){
+	printf("MMAP coping\n");
+	return SUCCESS;
 }
